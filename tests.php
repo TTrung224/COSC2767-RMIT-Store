1fad9dc468b34dfc082673df9aa9e56260d0a558
Trung Tran <s3891724@rmit.edu.vn>
2024-01-11 11:24:53 +0700

    update test file

--- tests.php
+++ tests.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>RMIT Store</title>
+    </head>
+    <body>
 <?php
 require_once 'functions.php';
 
@@ -132,5 +139,5 @@ if (!$link) {
     mysqli_query($link, "DELETE FROM store WHERE id='$testId1' OR id='$testId2';");
 }
 ?>
-
+    </body>
 </html>
